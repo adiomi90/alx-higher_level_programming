fdfc65721ae4c38255c9cc17fbfc8406475f02bf
adiomi90 <adiomi90@outlook.com>
2023-05-11 13:04:03 -0700

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,12 @@
 if __name__ == "__main__":
     from sys import argv
 
-    count = len(argv) 
+    count = len(argv)
     if count == 1:
         print("0 arguments.")
     elif count == 2:
         print("1 argument:")
     else:
         print("{:d} arguments:".format(count - 1))
-
     for x in range(1, count):
-        print("{:d} : {:d}".format(x, argv[x))
+        print("{:d}: {:d}".format(x, argv[x))

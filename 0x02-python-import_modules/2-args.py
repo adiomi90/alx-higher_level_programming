6cca36f32d5b99dc330c8ddffbec8931c82e6f93
adiomi90 <adiomi90@outlook.com>
2023-05-11 12:49:44 -0700

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import args
+
+    count = len(argv)
+    if count == 1:
+        print("0 argument.")
+    elif count == 2:
+        print("1 argument.")
+    else:
+        print("{:d} arguments:".format(count - 1))
+
+    for x in range(1, count):
+        print("{:d} : {:d}".format(x, argv[x))

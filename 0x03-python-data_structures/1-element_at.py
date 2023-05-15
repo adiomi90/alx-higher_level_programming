f485a919ab036926671287248c73855f22884b0b
adiomi90 <adiomi90@outlook.com>
2023-05-15 05:14:12 -0700

    element at

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    elif idx >(len(myList)):
+    elif idx >(len(myList) - 1):
         return None
     else:
         return my_list[idx]

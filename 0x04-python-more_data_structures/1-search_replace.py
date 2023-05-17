499ed93a5185395cddcb1cec4c15350ea3d459e0
adiomi90 <adiomi90@outlook.com>
2023-05-17 01:31:14 -0700

    search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/bin/usr/python3
+def search_replace(my_list, search, replace)
+        new_list = []
+        for i in my_list:
+            if i == search:
+                new_list.append(replace)
+            else:
+                new_list.append(i)
+        retun new_list

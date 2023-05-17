65421583f70b60baed2f4281a9da4fe06d305f87
adiomi90 <adiomi90@outlook.com>
2023-05-17 06:32:37 -0700

    search replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = [repalce for search in my_list]
-    return new_list
+    the_list = []
+    for x in my_list:
+        if x == search:
+            the_list.append(replace):
+         else:
+            the_list.append(x)
+    return x

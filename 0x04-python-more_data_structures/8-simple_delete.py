db7a467e8bd370fcf9974a3f04fd88dff1381e31
adiomi90 <adiomi90@outlook.com>
2023-05-17 17:18:23 -0700

    del key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     for x in a_dictionary:
-        if x == key:
-            dictionary[x].pop()
+        del dictionary[key]
     return a_dictionary

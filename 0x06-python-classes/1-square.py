a9cbfac61f73dc7ba48fb2f3ab8541e893d98228
adiomi90 <adiomi90@outlook.com>
2023-05-30 02:40:58 -0700

    class square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """shwos a square"""
+
+    def __init__(self, size):
+        """constructor"""
+
+        self.__size = size

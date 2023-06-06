9c4714113be52b5e504e8ca559575ee41e6fe67a
adiomi90 <adiomi90@outlook.com>
2023-06-05 18:33:32 -0700

    calculate perimeter and area

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -15,6 +15,7 @@ class Rectangle:
         self.width = width
         self.height = height
 
+
     @property
     def width(self):
         """defines the width"""
@@ -35,7 +36,8 @@ class Rectangle:
         return self.__height
 
 
-    @height(self, value):
+    @height.setter
+    def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:

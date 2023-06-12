130b6211fcf8063b72dfe2b5f0a8bb5447d6e166
adiomi90 <adiomi90@outlook.com>
2023-06-12 06:42:40 -0700

    returns a instance of a class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,6 +4,6 @@
 
 def is_same_class(obj, a_class):
     """Checks if an object is exactly an instane"""
-    if type(obj) is a_class :
+    if type(obj) is a_class:
         return True
     return False

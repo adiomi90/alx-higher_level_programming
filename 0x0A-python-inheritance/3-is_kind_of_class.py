e27238df99b6371486438922da2d2d15f378b609
adiomi90 <adiomi90@outlook.com>
2023-06-12 07:38:38 -0700

    is kidn of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class and inheritance class"""
+
+
+def is_kind_of_class(obj_test, a_class):
+    """checks if the object is an instance
+       of the class 
+    """
+    return isinstance(obj_test, a_class)

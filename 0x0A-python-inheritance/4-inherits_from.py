d1ff8af7a84ec4aea345c0047bc3f07c23f0a64e
adiomi90 <adiomi90@outlook.com>
2023-06-12 07:50:15 -0700

    inheritance directly or indirectly

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ def inherits_from(obj, a_class):
     """Checks if the object is an instance of a class that inherited
        (directly or indirectly) from the specified class
     """
-    return issubclass(type(obj), a_class) and type(obj) is not a_class)
+    return issubclass(type(obj), a_class) and type(obj) is not a_class

b75113e52d58b173ac2db9dcd3611a305967d810
adiomi90 <adiomi90@outlook.com>
2023-06-13 09:28:08 -0700

    to json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that returns the JSON representation"""
+import json
+
+
+def to_json_string(my_test):
+    """Function returns the JSON representation of the string"""
+
+    return json.dumps(my_test)

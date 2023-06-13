840dd4905ecec7f9eff8132ece756fa905fc8839
adiomi90 <adiomi90@outlook.com>
2023-06-13 09:19:50 -0700

    write a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module to write a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """Function that writes a string to a file"""
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)

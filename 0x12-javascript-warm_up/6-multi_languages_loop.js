d9367e24ae273802bcde044cbf7621ca75eb954e
adiomi90 <adiomi90@outlook.com>
2023-08-14 16:40:29 -0700

    loops

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Prints using loops
+
+const myLanguages =['C is fun', 'Python is cool', 'JavaScript is amazing']
+
+let i = 0;
+while (i < myLanguages.length){
+  console.log(myLanguages[i]);
+  i++;
+}

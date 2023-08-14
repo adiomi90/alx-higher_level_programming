86d79447c7023a58639cfce4814750a6a601cfed
adiomi90 <adiomi90@outlook.com>
2023-08-14 16:04:23 -0700

    argsv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-//if else statement using process.argv
+//if else statement using process argv
 
-if (process.argv.length < 3){
+if (process.argv.length < 3) {
 	console.log('No argument');
-} else if (process.argv.length == 3){
+} else if (process.argv.length === 3) {
 	console.log('Argument found');
-} else if(process.argv.length > 3){
+} else if(process.argv.length > 3) {
 	console.log('Argument found');
 }

6da8b7894a5c84a812eb1453de52041c827ec82e
adiomi90 <adiomi90@outlook.com>
2023-08-14 16:24:27 -0700

    check args without length

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,7 +4,18 @@
 
 const myVar = process.argv[2];
 if (myVar === undefined) {
- console.log('No argument');
+  console.log('No argument');
 } else {
- console.log(myVar);
+  console.log(myVar);
+}
+
+#!/usr/bin/node
+
+// Prints the first argument passed to it
+
+const myArgs = process.argv[2];
+if (myArgs === undefined) {
+  console.log('No argument');
+} else {
+  console.log(myArgs);
 }

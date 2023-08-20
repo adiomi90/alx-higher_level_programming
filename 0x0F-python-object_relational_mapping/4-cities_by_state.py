2730299c8b25b484df995fc1f242908ef71842a4
adiomi90 <adiomi90@outlook.com>
2023-08-20 13:55:20 -0700

    join statement

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,10 @@ if __name__ == "__main__":
                            passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = conn.cursor()
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name =%s
+    cur.execute("""SELECT cities.id, cities.name, states.name
+                   FROM cities
+                   JOIN states
+                   ON cities.state_id = states.id
                   ORDER BY states.id ASC""", (sys.argv[4],))
     rows = cur.fetchall()
     for row in rows:

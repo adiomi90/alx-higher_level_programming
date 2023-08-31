f18d13ec3cac9c24e5ca502f5abb502f7f0f5ec2
adiomi90 <adiomi90@outlook.com>
2023-08-31 14:01:23 -0700

    remove files

--- 0x10-python-network_0/0-body_size.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#this script takes url andshows the body size
-
-curl -sI "$1" | grep Content-Length | cut -d ' ' -f 2
--- 0x10-python-network_0/1-body.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#This scirpt takes url, get request and show response body
-
-curl -Ls "$1"

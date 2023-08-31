222f27e0f899ba944e7ce11200acfccdad4d0f3d
adiomi90 <adiomi90@outlook.com>
2023-08-31 14:58:24 -0700

    advanced

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Sends a request to a URL passed as an argument, displays only the status code
-curl -s -o /dev/null -w "%{http_code}" "$1"
+curl -sL -H "content-type:application/json"  -d @"$2" -X POST "$1"

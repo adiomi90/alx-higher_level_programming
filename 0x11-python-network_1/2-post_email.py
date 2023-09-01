a646feb45c51aafe821efc0acf6daa075f11188e
adiomi90 <adiomi90@outlook.com>
2023-09-01 10:07:17 -0700

    post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,12 +7,13 @@ displays the body of the response
 from urllib import request, parse
 import sys
 
+
 def make_request():
     email = {'email': sys.argv[2]}
     post = parse.urlencode(email)
     with_ascii = post.encode('ascii')
-    request = request.Request(sys.argv[1], with_ascii)
-    with request.urlopen(request) as response:
+    req = request.Request(sys.argv[1], with_ascii)
+    with request.urlopen(req) as response:
         body = response.read()
         print(body.decode('utf-8'))
 

d90f9c1db6ee679da7283848a7a2e1dc30e5347c
adiomi90 <adiomi90@outlook.com>
2023-09-01 06:46:12 -0700

    make request

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,11 +6,9 @@ the value of the X-Request-Id variable
 import urllib.request
 import sys
 
-def make_request():
-        with urllib.request.urlopen(sys.argv[1]) as response:
-            html = response.info()
-            print(html.get('X-Request-Id'))
 
 
-if __name == "__main__":
-    make_request()
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = response.info()
+        print(html.get('X-Request-Id'))

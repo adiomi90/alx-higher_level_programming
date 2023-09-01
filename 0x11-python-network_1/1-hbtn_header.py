06f3da13619c003425f131344ad8dbaa0d099ff7
adiomi90 <adiomi90@outlook.com>
2023-09-01 06:36:37 -0700

    header request

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,10 +6,14 @@ of X-Request_Id variable found in header
 import urllib.request
 import sys
 
+
+
 def make_request():
     with urllib.request.urlopen(sys.argv[1]) as response:
         html = response.info()
         print(html.get('X-Request-Id'))
+
+
 
 
 if __name__ == "__main__":

5d2665c2f58a0a5c65a7ffdc1b304184b970b8d7
adiomi90 <adiomi90@outlook.com>
2023-09-01 06:51:49 -0700

    read X-Request-Id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """
-This  script take a URL, sends a request and displays
-the value of the X-Request-Id variable
+Python script that takes in a URL, sends a request and displays
+the value of the X-Request-Id variable found in the header
 """
 import urllib.request
 import sys
-
 
 
 if __name__ == "__main__":

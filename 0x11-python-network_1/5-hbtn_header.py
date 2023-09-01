d1f4d82b05cc031986ca3648a62f21dc0bbd3e92
adiomi90 <adiomi90@outlook.com>
2023-09-01 12:12:53 -0700

    request X-request-id

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Python script that sends a request to the URL and
+displays the value of a variable in the response header
+"""
+import requests
+import sys
+
+
+def make_request():
+    try:
+        req = requests.get(sys.argv[1])
+        print(req.headers['X-Request-Id'])
+    except:
+        pass
+
+if __name__ == "__main__":
+    make_request()

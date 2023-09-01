9cf8fd2a626d61144a0f8484ceea4ff25d18953c
adiomi90 <adiomi90@outlook.com>
2023-09-01 12:00:40 -0700

    using request package

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,18 +2,16 @@
 """
 Python script that fetches an URL with requests package
 """
-import requests
+import requets
 
 
 def make_request():
-    the_request = requests.get('https://alx-intranet.hbtn.io/status')
-    text = the_request.text
-
+    request = requests.get('https://alx-intranet.hbtn.io/status')
+    text = request.text
     print('Body response:')
-    print(f'\t- type: {type(the_request)}')
-    print(f'\t- content: {text}')
+    print('\t- type: {type(text)}')
+    print('\t- content: {text}')
 
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     make_request()

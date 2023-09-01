9e3d6016cdec83fc5ffaf0ab21b7802b5bc32eba
adiomi90 <adiomi90@outlook.com>
2023-09-01 06:44:26 -0700

    make request

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,7 +7,6 @@ import urllib.request
 import sys
 
 def make_request():
-    if __name__ == "__main__":
         with urllib.request.urlopen(sys.argv[1]) as response:
             html = response.info()
             print(html.get('X-Request-Id'))

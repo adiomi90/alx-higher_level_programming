4fea0f7647022ff80791252dc65232230b8e84b4
adiomi90 <adiomi90@outlook.com>
2023-11-13 12:57:36 -0800

    print x number of times using first argument

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,11 +4,10 @@
 
 const firstArg = parseInt(process.argv[2]);
 
-if firstArg {
+if (isNaN(firstArg)) {
+  console.log('Missing number of occurrences');
+} else {
   for (let i = 0; i < firstArg; i++) {
     console.log('C is fun');
   }
-} else {
-  console.log("Missing number of occurrences");
 }
-

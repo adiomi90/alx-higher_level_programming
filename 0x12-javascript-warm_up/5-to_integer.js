ca596006f6be5f9811fa0ee65f86362ec0cbe7e0
adiomi90 <adiomi90@outlook.com>
2023-11-13 12:07:36 -0800

    prints a first number converted to int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,8 +3,8 @@
 // prints first argument converted to int
 
 const firstArg = parseInt(process.argv[2]);
-if firstArg {
-  console.log(firstArg);
+if (firstArg) {
+  console.log("My number:" + firstArg);
 } else {
   console.log('Not a number');
 }

65b936f1bdd12f44d31fc4e87810f207cc6fc4e6
adiomi90 <adiomi90@outlook.com>
2023-11-13 12:15:49 -0800

    prints array of strings

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// print an array of strings
+
+const multi_lan = ['C is fun', 'Python is cool', 'JavaSCript is amazing'];
+
+let i = 0
+while ( i < multi_lan.length){
+  console.log(multi_lan[i]);
+  i++;
+}

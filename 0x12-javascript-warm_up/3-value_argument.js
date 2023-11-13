8e55a0dcc07acc4b8366b79caad8c7e41f39c821
adiomi90 <adiomi90@outlook.com>
2023-11-13 11:50:48 -0800

    prints first argument passed

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// Script prints the first argument passed to it
+
+const firstArg = process.argv[2];
+if (firstArg === undefined) {
+  console.log('No argument');
+} else {
+  console.log(firstArg);
+}

83276e418b5b71c40cda7476462b1c9c1dda9742
adiomi90 <adiomi90@outlook.com>
2023-11-13 11:40:19 -0800

    print arguments passed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 // script that prints argument passed to it
 
 if (process.argv.length < 3) {
-	console.log('No argument');
+  console.log('No argument');
 } else if ( process.argv.length === 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else if (process.argv.length > 3) {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }

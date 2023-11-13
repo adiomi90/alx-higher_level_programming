7f072d185e6685979f96c886ef286ea346bb4e65
adiomi90 <adiomi90@outlook.com>
2023-11-13 11:27:44 -0800

    print multi lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-// Script that print multi lines
+//Script that prints multi lines
 
-console.log('C is fun')
-console.log('Python is cool')
-console.log('JavaScript is amazing')
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");

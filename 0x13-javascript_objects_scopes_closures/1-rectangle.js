118b1bbbc1767f00827bff2b566314b75c8dd50f
adiomi90 <adiomi90@outlook.com>
2023-11-14 14:49:54 -0800

    rectangle class with width and height

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,11 +2,10 @@
 
 // class rectangle with width and height
 
-
 class Rectangle {
   constructor (w, h) {
     this.width = w;
     this.height = h;
   }
 }
-module.exports = Rectangle;  
+module.exports = Rectangle;

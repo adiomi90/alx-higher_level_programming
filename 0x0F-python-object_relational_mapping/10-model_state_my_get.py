9d3eb0892a49b23e1dbac5b8738cb611a507bab8
adiomi90 <adiomi90@outlook.com>
2023-11-19 15:17:08 -0800

    prints state object with name passed

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Script that prints the State Onject with the name as an arg
+from the database"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).all()
+
+    for state in states:
+        if state.name == sys.argv[4]:
+            print("{}".format(state.id))
+        else:
+            print("Not found")

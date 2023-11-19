146c243abdaf6460c463500fd339f0c2d037604e
adiomi90 <adiomi90@outlook.com>
2023-11-19 13:40:47 -0800

    safe filter SQL injections

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,8 +10,8 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY name=%s\
-                 ORDER BY states.id ASC".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name=%s\
+                 ORDER BY states.id ASC", (sys.argv[4], ))
     rows = cur.fetchall()
     for row in rows:
         print(row)

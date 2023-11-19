1dbee238a85a6d4035ce543ce44e176f4275b72c
adiomi90 <adiomi90@outlook.com>
2023-11-19 13:08:06 -0800

    filter by a starting letter N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""list all states with a name stating 
+   with upper case N in the database"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost",
+                        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id ASC;""")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)

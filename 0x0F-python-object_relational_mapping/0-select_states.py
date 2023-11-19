3deb6bb2211ad611969589aa1901cdfb5a38a95b
adiomi90 <adiomi90@outlook.com>
2023-11-19 12:40:18 -0800

    select state from db

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,17 +6,17 @@ import sys
 
 if __name__ == "__main__":
 
-  user = sys.argv[1]
-  password = sys.argv[2]
-  dbname = sys.argv[3]
+    user = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
 
-  db = MySQLdb.connect(host="localhost", port=3306,
+    db = MySQLdb.connect(host="localhost", port=3306,
                        user=user, passwd=password, db=dbname)
 
-  cur = db.cursor()
-  cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
-  rows = cur.fetchall()
-  for row in rows:
-    print(row)
-  cur.close()
-  db.close()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()

034cf2a3cd9b6e10ad9a41bb250f12b56913077c
adiomi90 <adiomi90@outlook.com>
2023-11-19 14:47:40 -0800

    use state as argument

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,7 +8,8 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3])
     cur = db.cursor()
     state = sys.argv[4]
     cur.execute("SELECT cities.name FROM cities INNER JOIN states\

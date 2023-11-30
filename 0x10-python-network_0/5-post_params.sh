76177e5b31b79a42e9f6f1dc47b93d80ed405c82
adiomi90 <adiomi90@outlook.com>
2023-11-30 12:19:59 -0800

    post request

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #post request to pass url and displys body response
-curl -s -X POST -d "email=test@gmail.com&subject=I will alwasy be here for PLD"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"

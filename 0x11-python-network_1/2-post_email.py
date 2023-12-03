50d0d13e98262c285f2b1f4bfd0c2fe066deb9c1
adiomi90 <adiomi90@outlook.com>
2023-12-03 01:35:34 -0800

    post request with email passed parameter

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,34 @@
+"""
+The script takes a url and an email, sends a
+Post request with email as paramter and
+displays body response
+"""
+import sys
+from urllib import parse
+from urllib.request import Request, urlopen
+
+
+def post_request_url(url, email):
+    try:
+        values = {"email": email}
+        data = parse.urlencode(values).encode("ascii")
+        request = Request(url, data, headers={'User-Agent': 'Mozilla/5.0'})
+        with urlopen(request) as response:
+            content = response.read()
+        return content
+    
+    except Exception as e:
+        print(f"Error {e}")
+        return None
+
+
+def main():
+    url = sys.argv[2]
+    email = sys.argv[3]
+
+    request = post_request_url(url, email)
+    print(request.decode("utf-8"))
+
+
+if __name__ == "__main__":
+    main()

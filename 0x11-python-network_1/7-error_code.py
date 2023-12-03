863397792dcea26120d4f3dd515c44674d08cfbf
adiomi90 <adiomi90@outlook.com>
2023-12-03 12:21:15 -0800

    request url with error handling using requests package

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,6 +4,7 @@
     sends a requewst to the URL and display body
     response
 """
+
 from sys import argv
 from requests import get, exceptions
 
@@ -21,15 +22,12 @@ def get_request(url):
 def main():
     url = argv[1]
     request = get_request(url)
-    
-    if request is None:
-        print("Request failed")
-        return
 
-    if request.status_code >= 400:
-        print(f"Error code: {request.status_code}")
-    else:
-        print(request.text)
+    if request is not None:
+        if request.status_code >= 400:
+            print(f"Error code: {request.status_code}")
+        else:
+            print(request.text)
 
 
 if __name__ == "__main__":

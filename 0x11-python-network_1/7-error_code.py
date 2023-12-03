ee231aeb13d2d200c13f21b6fe6cae2f8f2c214c
adiomi90 <adiomi90@outlook.com>
2023-12-03 11:51:12 -0800

    get request with error handling using requests package

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    A python scipt that uses the requests packaage
+    sends a requewst to the URL and display body
+    response
+"""
+from sys import argv
+from requests import get, exceptions
+
+
+def get_request(url):
+    try:
+        response = get(url)
+        response.raise_for_status()
+        return response
+    except exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return None
+
+
+def main():
+    url = argv[1]
+    request = get_request(url)
+
+    if request is not None:
+        if request.status_code >= 400:
+            print(f"Error code: {request.status_code}")
+        else:
+            print(request.text)

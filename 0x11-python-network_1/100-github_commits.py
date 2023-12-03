44bb6a8540eaf5a6ed0b25966ae03a08def76203
adiomi90 <adiomi90@outlook.com>
2023-12-03 12:52:46 -0800

    github commit

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -18,5 +18,5 @@ if __name__ == "__main__":
         for i in range(0, 10):
             print("{}: {}".format(json_o[i].get('sha'), json_o[i].get('commit')
                                   .get('author').get('name')))
-    except:
-        pass
+    except Exception as e:
+        print(f"Error: {e})

4127a3d8081defbfb3593e354839fa2bd6f395d9
adiomi90 <adiomi90@outlook.com>
2023-12-02 21:31:35 -0800

    fetch the url and return response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,28 +1,19 @@
 #!/usr/bin/bash
 """script that fetches from the url https://alx-intranet.hbtn.io/status"""
 from urllib.request import urlopen, Request
-# headers={'User-Agent': 'Mozilla/5.0'}
 
 
-def make_request(url):
-    try:
-        request = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-        with urlopen(request) as response:
-            content = response.read()
-        return content
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
+def main:
 
+    request = Request("https://alx-intranet.hbtn.io/status")
+    with urlopen(request) as response:
+        content = response.read()
 
-def main():
-    content = make_request("https://alx-intranet.hbtn.io/status")
-
-    if content:
-        print("Body response:")
-        print(f"\t- type: {type(content)}")
-        print(f"\t- content: {content}")
-        print(f"\t- utf8 content: {content.decode('utf-8')}")
+        if content:
+            print("Body response:")
+            print(f"\t- type: {type(content)}")
+            print(f"\t- content: {content}")
+            print(f"\t- utf8 content: {content.decode('utf-8')}")
 
 
 if __name__ == '__main__':

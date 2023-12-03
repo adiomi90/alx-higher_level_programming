2c50c0b1bfc2f80c8e160fe4275a5e12cb1510ba
adiomi90 <adiomi90@outlook.com>
2023-12-03 04:27:32 -0800

    fetching data using requests package

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    A python script that send requests
+    and print the body response
+"""
+
+import requests
+
+
+def main():
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
+
+
+if __name__ == "__main__":
+    main()
